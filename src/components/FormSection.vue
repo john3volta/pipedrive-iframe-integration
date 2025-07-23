6202671170204af32b9b62620b8b6f0e75f20e94
john3volta <workbox_1@tutanota.com>
2025-07-23 15:12:22 +0200

    add: FormSection component

---
+++ src/components/FormSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="form-section">
+    <h3 class="form-section__title">{{ title }}</h3>
+    <div class="form-section__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string
+}
+
+defineProps<Props>()
+</script> 
