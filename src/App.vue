cf96a0a31eb3822ed0ebbc0fa116510e9742317c
john3volta <workbox_1@tutanota.com>
2025-07-23 13:27:09 +0200

    add: Vue app structure with main form component

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <JobForm />
+  </div>
+</template>
+
+<script setup lang="ts">
+import JobForm from './components/JobForm.vue'
+</script> 
---
+++ src/components/JobForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="form">
+    <div class="form__container">
+      <ClientDetails />
+      <JobDetails />
+      <ServiceLocation />
+      <Scheduled />
+    </div>
+    <div class="form__actions">
+      <button class="form__btn form__btn--create">Create Job</button>
+      <button class="form__btn form__btn--save">Save Info</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Form logic
+</script> 
