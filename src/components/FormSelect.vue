983e7676713f0663f93447397f8f6bd56471e08e
john3volta <workbox_1@tutanota.com>
2025-07-23 15:08:50 +0200

    add: FormSelect component

---
+++ src/components/FormSelect.vue
@@ -0,0 +1,36 @@
+<template>
+  <select 
+    class="form-field__input" 
+    :value="modelValue"
+    @change="$emit('update:modelValue', ($event.target as HTMLSelectElement).value)"
+    :required="required"
+  >
+    <option value="">{{ placeholder || 'Select option' }}</option>
+    <option 
+      v-for="option in options" 
+      :key="option.value" 
+      :value="option.value"
+    >
+      {{ option.label }}
+    </option>
+  </select>
+</template>
+
+<script setup lang="ts">
+interface Option {
+  value: string
+  label: string
+}
+
+interface Props {
+  modelValue: string
+  options: Option[]
+  required?: boolean
+  placeholder?: string
+}
+
+defineProps<Props>()
+defineEmits<{
+  'update:modelValue': [value: string]
+}>()
+</script> 
