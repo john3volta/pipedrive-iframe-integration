e30c8ee17c23e97428a73abfe11bceaba1d01dd6
john3volta <workbox_1@tutanota.com>
2025-07-23 15:10:30 +0200

    add: FormField component

---
+++ src/components/FormField.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="form-field">
+    <label class="form-field__label">{{ label }}</label>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  label: string
+}
+
+defineProps<Props>()
+</script> 
