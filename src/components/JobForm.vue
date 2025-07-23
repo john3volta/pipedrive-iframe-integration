f47aa41b405f894b0992ddb8016c7d035687dddd
john3volta <workbox_1@tutanota.com>
2025-07-23 15:39:11 +0200

    fix: add missing component imports and create index.html

--- src/components/JobForm.vue
+++ src/components/JobForm.vue
@@ -23,6 +23,10 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { jobTypes, jobSources, areaOptions, technicians } from '../data/formOptions'
+import ClientDetails from './ClientDetails.vue'
+import JobDetails from './JobDetails.vue'
+import ServiceLocation from './ServiceLocation.vue'
+import Scheduled from './Scheduled.vue'
 
 const jobType = ref('')
 </script> 
