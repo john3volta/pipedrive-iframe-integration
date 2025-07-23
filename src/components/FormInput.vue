bc991efdacced10b7111f1e05e9c04207b5a6a35
john3volta <workbox_1@tutanota.com>
2025-07-23 15:08:20 +0200

    add: FormInput component

---
+++ src/components/FormInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <input 
+    class="form-field__input" 
+    :type="type"
+    :value="modelValue"
+    @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+    :required="required"
+    :placeholder="placeholder"
+  />
+</template>
+
+<script setup lang="ts">
+interface Props {
+  modelValue: string
+  type?: string
+  required?: boolean
+  placeholder?: string
+}
+
+defineProps<Props>()
+defineEmits<{
+  'update:modelValue': [value: string]
+}>()
+</script> 
