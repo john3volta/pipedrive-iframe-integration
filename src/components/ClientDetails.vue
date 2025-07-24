3e76715ed8dd02de7514cb447eb85cc72543a5ee
john3volta <workbox_1@tutanota.com>
2025-07-24 15:13:04 +0200

    refactor: improve ClientDetails formatting

--- src/components/ClientDetails.vue
+++ src/components/ClientDetails.vue
@@ -2,14 +2,35 @@
   <FormSection title="Client details">
     <div class="form-row">
       <FormField>
-        <FormInput v-model="firstName" name="firstName" type="text" placeholder="First name" autocomplete="given-name" required />
+        <FormInput 
+          v-model="firstName"
+          name="firstName" 
+          type="text" 
+          placeholder="First name" 
+          autocomplete="given-name" 
+          required 
+        />
       </FormField>
       <FormField>
-        <FormInput v-model="lastName" name="lastName" type="text" placeholder="Last name" autocomplete="family-name" required />
+        <FormInput 
+          v-model="lastName"
+          name="lastName" 
+          type="text" 
+          placeholder="Last name" 
+          autocomplete="family-name" 
+          required 
+        />
       </FormField>
     </div>
     <FormField>
-      <FormInput v-model="phone" name="phone" type="tel" placeholder="Phone" autocomplete="tel" required />
+      <FormInput 
+        v-model="phone"
+        name="phone" 
+        type="tel" 
+        placeholder="Phone" 
+        autocomplete="tel" 
+        required 
+      />
     </FormField>
     <FormField>
       <FormInput v-model="email" name="email" type="email" placeholder="Email (optional)" autocomplete="email" />
